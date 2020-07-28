3b68e733e494a7ddc98cf8a24cea99523e23e2bb
NhatNguyen <NhatNguyen@DESKTOP-1U7JQ76>
2020-07-28 23:04:38 +0700

    Cập nhật view đăng ký

--- resources/views/customer/layout/login.blade.php
+++ resources/views/customer/layout/login.blade.php
@@ -67,3 +67,61 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="signupModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle1"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="col-md-5 mx-auto">
+            <div class="myform form ">
+                <div class="logo mb-3">
+                    <div class="col-md-12 text-center">
+                        <h1 style="font-family: serif;">Đăng ký</h1>
+                    </div>
+                </div>
+                <form action="" method="post" name="login" id="form-signup">
+                    @csrf
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="exampleInputEmail1">Họ và tên đệm</label>
+                                <input type="text" required name="last_name" class="form-control" aria-describedby="emailHelp"
+                                       placeholder="Họ" >
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="exampleInputEmail1">Tên</label>
+                                <input type="text" name="first_name" required class="form-control" aria-describedby="emailHelp"
+                                       placeholder="Tên" >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="exampleInputEmail1">Địa chỉ email</label>
+                        <input type="email" name="email" class="form-control" aria-describedby="emailHelp"
+                               placeholder="Địa chỉ email" required>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="exampleInputEmail1">Mật khẩu</label>
+                        <input type="password" name="password" id="password_signup" class="form-control" aria-describedby="emailHelp"
+                               placeholder="Mật khẩu" required>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="exampleInputEmail1">Nhập lại mật khẩu</label>
+                        <input type="password" name="confirm_password-signup" id="confirm_password_signup" class="form-control" aria-describedby="emailHelp"
+                               placeholder="Nhập lại mật khẩu" required>
+                    </div>
+                    <div class="form-group">
+                        <p class="text-center">Tôi đồng ý với Bảo mật và Điều khoản hoạt động của Gemingear.vn</p>
+                    </div>
+                    <div class="col-md-12 text-center ">
+                        <button type="submit" id="register" class="btn btn-block mybtn btn-primary tx-tfm">Đăng ký
+                        </button>
+                    </div>
+                    <div class="col-md-12 notify" style="margin-top:10px;margin-bottom:0px;display:none;"></div>
+                    <div class="col-md-12" style="margin-top: 15px;">
+                            <hr class="hr-or">
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
