5029070c9c95f392b0ccff7ad3b9990f057b00f3
Dien <dienuaf02@gmail.com>
2020-07-30 13:33:48 +0700

    Update Customer.php

--- app/Http/Controllers/Customer.php
+++ app/Http/Controllers/Customer.php
@@ -49,7 +49,7 @@ class Customer extends Controller
         return redirect('/');
     }
 
-    
+
     //Đăng ký
     public function signup(Request $res)
     {
@@ -77,13 +77,63 @@ class Customer extends Controller
     			'link' => $res->root() . '/customer/update/' . $res->input('email'),
     			'email' => $res->input('email'),
     	);
-    	
+
     	if ($this->users->save()) {
     		Mail::to($res->input('email'))->send(new SendMail('Xác nhận thông tin địa chỉ email tại Gemingear.vn', $message));
     		return response()->json(['success' => 'Đăng ký thành công vui lòng kiểm tra email của bạn']);
     	} else {
     		return response()->json(['success' => 'Đăng ký thất bại! Xin kiểm tra lại']);
     	}
+    }
+    public function sendMailForgotPass(Request $request)
+    {
+        // Lấy thông tin user từ email người dùng nhập vào
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        //thêm data vào table passwordReset
+        $passwordReset = PasswordReset::updateOrCreate([
+            'email' => $user->email,
+        ], [
+            'token' => Str::random(60),
+        ]);
+        //Tạo data chứa token để về mail cho người dùng
+        $message = array(
+            'token' => $passwordReset->token,
+            'email' => $request->input('email'),
+        );
+        //Nếu add data vào table thành công thì gửi mail
+        if ($passwordReset) {
+//            $user->notify(new Customer($passwordReset->token));
+            Mail::to($request->input('email'))->send(new SendTokenMail('Xác nhận thông tin địa chỉ email tại Gemingear.vn', $message));
+            return response()->json(['success' => 'Đăng ký thành công vui lòng kiểm tra email của bạn']);
+
+        }
+        return response()->json([
+            'message' => 'We have e-mailed your password reset link!'
+        ]);
+
+    }
+//
+    public function reset(Request $request)
+    {
+        //Lấy token người dùng nhập vào kiểm tra có tồn tại trong database
+        $token = $request->token;
+        $passwordReset = PasswordReset::where('token', $token)->firstOrFail();
+        //Nếu thời gian lớn hơn 12h thì xóa data trong database và trả về lỗi time out
+        if (Carbon::parse($passwordReset->updated_at)->addMinutes(720)->isPast()) {
+            $passwordReset->delete();
+
+            return response()->json([
+                'message' => 'This password reset token time out.',
+            ], 422);
+        }
+        // Change password
+        $user = User::where('email', $passwordReset->email)->firstOrFail();
+        $newPassword = Hash::make($request->password);
+        $updatePasswordUser = $user->update(['password' => $newPassword]);
+        $passwordReset->delete();
+
+        return redirect('/');
     }
     //Xác thực email
     public function update($email)
@@ -102,5 +152,5 @@ class Customer extends Controller
     			're_password.required' => 'Password không trùng khớp'
     	];
     }
-    
+
 }
