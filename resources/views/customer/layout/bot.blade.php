b850f2fa4c633e7a2cff2e75ac709dfecbb29a26
quyen <lelamphuongquyen@gmail.com>
2020-08-03 23:33:54 +0700

    Quyen - commit javascript cho phần hiện thông báo thành công hay thất bại

--- resources/views/customer/layout/bot.blade.php
+++ resources/views/customer/layout/bot.blade.php
@@ -7,3 +7,40 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
     crossorigin="anonymous"></script>
+ 
+<script type="text/javascript">
+    $(document).ready(function () {
+            @if(Session:: has('fail'))
+{{--        thư viện hổ trợ hiển thị mockup    --}}
+        const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+        Toast.fire({
+            type: 'error',
+            title: '{{ Session:: get('fail') }}'
+        });
+            @endif
+            @if(Session:: has('success'))
+        const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+            });
+        Toast.fire({
+            type: 'success',
+            title: '{{ Session:: get('success') }}'
+        });
+        @endif
+        @if(Session:: has('login'))
+        Swal.fire({
+            type: 'success',
+            title: '{{ Session:: get('login') }}',
+            showConfirmButton: false,
+        });
+        @endif
+    });
+</script>
