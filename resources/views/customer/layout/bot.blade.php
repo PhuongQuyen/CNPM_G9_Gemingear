6ecc6e8aa79216d7636826e15e5f3c22bcb37372
nhatnguyen97EX <55454170+nhatnguyen97EX@users.noreply.github.com>
2020-08-04 20:37:12 +0700

    Cập nhật script signup

--- resources/views/customer/layout/bot.blade.php
+++ resources/views/customer/layout/bot.blade.php
@@ -44,3 +44,41 @@
         @endif
     });
 </script>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    
+    $(document).ready(function () {
+        $('#form-signup').submit(function (e) {
+            e.preventDefault();
+            $('.alert-danger').remove();
+            $('.alert-success').remove();
+            $.ajax({
+                type: "post",
+                url: "/customer/signup",
+                data: {
+                    last_name: $("input[name=last_name]").val(),
+                    first_name: $("input[name=first_name]").val(),
+                    email:  $("#signupModalCenter input[name=email]").val(),
+                    password: $("#signupModalCenter input[name=password]").val(),
+                },
+                dataType: "json",
+                success: function (data) {
+                    if(typeof data.errors !== "undefined"){
+                        jQuery.each(data.errors, function(key, value){
+                  			$('.notify').show();
+                  			$('.notify').append('<div class="alert error-signup alert-danger"><p>'+value+'</p></div>');
+                  	    });
+                    }else{
+                        $('.notify').show();
+                  		$('.notify').append('<div class="alert alert-success"><p>'+data.success+'</p></div>');
+                        $("#form-signup")[0].reset();
+                    }
+                }
+            });
+        });
+    });
+</script>
